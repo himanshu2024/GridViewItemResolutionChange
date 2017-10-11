acbc4d591ea353da995f1a4eba9deb820baf8c88
Himanshu Chaurasiya <himanshu@divum.in>
2017-10-11 12:49:43 +0530

    first commit

---
+++ GridViewItemResolutionChange/MainViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridViewItemResolutionChange
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Item> _items;
+       
+
+        public ObservableCollection<Item> Items
+        {
+            get { return _items; }
+            set { SetProperty(ref _items, value); }
+        }
+
+        /*This method will apply a size to each item in the view model.*/
+        public void Measure(double size)
+        {
+            for (int i = 0; i < Items.Count; i++)
+            {
+                var item = Items[i];
+                item.Size = size;
+            }
+        }
+
+        public MainViewModel()
+        {
+            Items = new ObservableCollection<Item>();
+
+            var random = new Random();
+            for (int x = 1; x <= 30; x++)
+            {
+                Items.Add(new Item()
+                {
+                    Name = string.Format("Item {0}", x),
+                    Color = String.Format("#FF{0:X6}", random.Next(0x1000000))
+                });
+            }
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] String propertyName = null)
+        {
+            if (Equals(storage, value)) return false;
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler eventHandler = PropertyChanged;
+            if (eventHandler != null)
+            {
+                eventHandler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
